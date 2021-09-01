bb1c98d0a7cb7e66a286e47e758fb12c822cbaae
Gyeo1 <rlatjdruf99@naver.com>
2021-09-01 15:11:41 +0900

    확인용

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,2 +1,8 @@
+from django.forms import fields
 from .models import User
 from django import forms
+
+class SignupForm(forms.ModelForm):#로그인을 구현하는 form이 필요
+    class Meta:
+        model=User
+        fields=['username','password']
--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,13 @@
+from accounts.forms import SignupForm
 from django.shortcuts import render
 
 # Create your views here.
+
+def signup(request):
+    if request.method =='POST':
+        form=  SignupForm(request.POST)
+    else:
+        form=SignupForm()
+    return render(request,'accounts/signup_form.html',{
+        'form':form
+    })
