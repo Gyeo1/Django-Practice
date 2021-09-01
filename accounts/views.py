273908730464f6981d7771d12f17a6bbc881564f
Gyeo1 <rlatjdruf99@naver.com>
2021-09-01 17:49:21 +0900

    next_URL판단하고 redirect

--- accounts/views.py
+++ accounts/views.py
@@ -9,7 +9,8 @@ def signup(request):
         if form.is_valid():
             user=form.save()
             messages.success(request,"회원 가입을 환영합니다.")
-            return redirect("root")
+            next_url=request.GET.get('next','/')#만약 next라는 url이 없다면 root 로
+            return redirect(next_url)
     else:
         form=SignupForm()
     return render(request,'accounts/signup_form.html',{
