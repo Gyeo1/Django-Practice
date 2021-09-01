836440236c3b1ced93661339c05fd417e79c786e
Gyeo1 <71872133+Gyeo1@users.noreply.github.com>
2021-09-01 15:08:06 +0900

    Update common.py

--- askcompany/settings/common.py
+++ askcompany/settings/common.py
@@ -9,7 +9,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__fil
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '6#w6*jv-eys296tc)3@-5=z421tave^f-y*pmefbh2_6*31+6e'
+SECRET_KEY = 
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
