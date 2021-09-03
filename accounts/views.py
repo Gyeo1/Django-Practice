36e6549872b9efc3cb84e3ad48f318ee729179ef
Gyeo1 <rlatjdruf99@naver.com>
2021-09-03 13:36:11 +0900

    회원 가입 후 로그인 page로 이동

--- accounts/views.py
+++ accounts/views.py
@@ -2,6 +2,7 @@ from re import template
 from accounts.forms import SignupForm
 from django.shortcuts import redirect, render
 from django.contrib import messages
+from django.contrib.auth import login as auth_login
 from django.core.mail import send_mail,BadHeaderError
 from django.http import HttpResponse
 from django.contrib.auth.views import LoginView, logout_then_login #로그인 전용 CBV
@@ -14,6 +15,7 @@ def signup(request):
         if form.is_valid():
             signed_user=form.save()
             #중요한거? signed_user는 forms.py의 SignupForm 내용을 저장, SignupForm Model.py의 User를 계승함!
+            auth_login(request,signed_user) #회원 가입하자마자 로그인
             messages.success(request,"회원 가입을 환영합니다.")
             # signed_user.send_welcome_email() #FIXME: Celery로 처리하는 것을 추천?
             #try파트는 회원 가입 성공시 이메일을 보내는 내용이다.
@@ -35,6 +37,9 @@ def signup(request):
 
 login=LoginView.as_view(template_name="accounts/login_form.html") #settings에 LOGIN_REDIRECT_URL=''로 설정해두기
                                     #안해놓으면 profile로 가서 profile.html만들기 전까지는 루트로 보낸다
+
+
 # logout=LogoutView.as_view(next_page="/",template_name="accounts/logout_form.html")
 def logout(request):
+    messages.success(request,"로그아웃 되셨습니다.")
     return logout_then_login(request)#로그아웃 하자마자 로그인 페이지로 보낸다!
