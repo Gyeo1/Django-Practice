41a1fa5ce177105c80f56b71b354acbe2e027573
Gyeo1 <rlatjdruf99@naver.com>
2021-09-03 13:28:57 +0900

    로그인/로그아웃 기능 구현

--- accounts/views.py
+++ accounts/views.py
@@ -1,8 +1,11 @@
+from re import template
 from accounts.forms import SignupForm
 from django.shortcuts import redirect, render
 from django.contrib import messages
 from django.core.mail import send_mail,BadHeaderError
 from django.http import HttpResponse
+from django.contrib.auth.views import LoginView, logout_then_login #로그인 전용 CBV
+from django.conf import settings
 # Create your views here.
 
 def signup(request):
@@ -29,3 +32,9 @@ def signup(request):
     return render(request,'accounts/signup_form.html',{
         'form':form
     })
+
+login=LoginView.as_view(template_name="accounts/login_form.html") #settings에 LOGIN_REDIRECT_URL=''로 설정해두기
+                                    #안해놓으면 profile로 가서 profile.html만들기 전까지는 루트로 보낸다
+# logout=LogoutView.as_view(next_page="/",template_name="accounts/logout_form.html")
+def logout(request):
+    return logout_then_login(request)#로그아웃 하자마자 로그인 페이지로 보낸다!
