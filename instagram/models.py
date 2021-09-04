c3206e5159ce3c581ade00034ddb4a3a22677729
Gyeo1 <rlatjdruf99@naver.com>
2021-09-04 15:44:11 +0900

    #태그를 읽어서 admin의 Tag_Set에 add하는 코드 작성

--- instagram/models.py
+++ instagram/models.py
@@ -1,16 +1,24 @@
 from django.db import models
 from django.conf  import settings
-
+import re
 # Create your models here.
 class Post(models.Model):
     author=models.ForeignKey(settings.AUTH_USER_MODEL,on_delete=models.CASCADE)#작성자 모델과 1:N관계 N측에 작성
-    photo=models.ImageField()
+    photo=models.ImageField(upload_to="instgram/post/%Y/%m/%d") #필드에 대해 적용되는게 아닌 파일 저장시 활용
     caption=models.CharField(max_length=500)
     tag_set=models.ManyToManyField('Tag',blank=True)#아래 태그 클래스와 M2M필드 연관관계 
     location=models.CharField(max_length=100)
 
     def __str__(self):
         return self.caption
+
+    def extract_tag_list(self): #이 함수는 tag의 모든 값을 뽑아오기 위해 사용한다.
+        tag_name_list= re.findall(r"#([a-zA-Z\dㄱ-힇]+)", self.caption) #알파벳과 한글을 뽑아낸다
+        tag_list=[]
+        for tag_name in tag_name_list: #tag_name_list의 리스트 값을 하나씩 가져옴
+            tag,_=Tag.objects.get_or_create(name=tag_name)#리스트 값을 tag란 이름의 값으로 만든다?
+            tag_list.append(tag)#tag_list에 값 할당
+        return tag_list
 
 class Tag(models.Model):
     name=models.CharField(max_length=50, unique=True)
--- instagram/views.py
+++ instagram/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render,redirect
 from django.contrib.auth.decorators import login_required
 from .forms import PostForm
 from django.contrib import messages
-
+from .models import Tag
 # Create your views here.
 @login_required
 def post_new(request):
@@ -12,8 +12,10 @@ def post_new(request):
             post=form.save(commit=False)#모델의 대한 필수필드의 값이 지정 안될 수도 있으므로 Flase로 설정
                             #왜나면 form에서 다른 필수 값들은 필드에 지정했지만 author를 지정 안함!! 
             post.author=request.user #따라서 여기서 유저를 지정해주고 save 구현
-            #post.tag_set은 blank=True라 나중에 처리해 보자.
             post.save()
+            #post.tag_set은 blank=True라 상관은 없지만 구현은 아래처럼 한다
+            post.tag_set.add(*post.extract_tag_list()) #태그 리스트 가져오는 함수를 tag_set에 추가
+
             messages.success(request,"포스팅을 저장 했습니다.")
             return redirect('/') #TODO: get_absolute_url을 나중에 활용한다.
     else:#GET요청 일 경우
