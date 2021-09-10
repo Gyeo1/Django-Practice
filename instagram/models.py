c9daa9532930f3b8ab646404469cbf97b715b904
Gyeo1 <rlatjdruf99@naver.com>
2021-09-10 14:15:23 +0900

    모델측에 like_user 모델 추가(related_name도 추가)

--- instagram/models.py
+++ instagram/models.py
@@ -1,5 +1,7 @@
+from typing import Callable
 from django.db import models
 from django.conf  import settings
+from django.db.models.deletion import CASCADE
 from django.urls import reverse
 import re
 # Create your models here.
@@ -10,14 +12,20 @@ class BaseModel(models.Model):
     class Meta:
         abstract=True
 
-
+#user
+#Post.object.filter(authot=user) ? 
+#user.post_set.all() ==>user.my_post_set.all()
 class Post(BaseModel):
-    author=models.ForeignKey(settings.AUTH_USER_MODEL,on_delete=models.CASCADE)#작성자 모델과 1:N관계 N측에 작성
+    author=models.ForeignKey(settings.AUTH_USER_MODEL,related_name='my_post_set'
+                                    ,on_delete=models.CASCADE)#작성자 모델과 1:N관계 N측에 작성
     photo=models.ImageField(upload_to="instgram/post/%Y/%m/%d") #필드에 대해 적용되는게 아닌 파일 저장시 활용
     caption=models.CharField(max_length=500)
     tag_set=models.ManyToManyField('Tag',blank=True)#아래 태그 클래스와 M2M필드 연관관계 
     location=models.CharField(max_length=100)
-    
+    like_user_set=models.ManyToManyField(settings.AUTH_USER_MODEL,blank=True,
+                                            related_name='like_post_set') 
+    #좋아요 기능을 위한 M2M Fields, 이렇게하면 위의 author의 setting 유저와 충돌이 일어난다. related_name사용
+    #하나만 related_name을 설정하지말고 겹치는 두개다 바꿔주는게 보기 좋다.    
 
     def __str__(self):
         return self.caption
@@ -40,3 +48,6 @@ class Tag(models.Model):
     def __str__(self):
         return self.name
 
+# class LikeUser(models.Model):#
+#     post=models.ForeignKey(Post, on_delete=models.CASCADE) #
+#     user=models.ForeignKey(settings.AUTH_USER_MODEL,on_delete=models.CASCADE)
