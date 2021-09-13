12e46dd05318190b3c4c143da951f9ee4052f72a
Gyeo1 <rlatjdruf99@naver.com>
2021-09-13 17:49:50 +0900

    rest_framework 사용해보기1

--- PracticeDjango/instagram/urls.py
+++ PracticeDjango/instagram/urls.py
@@ -1,3 +1,11 @@
+from django.urls import include, path
+from rest_framework.routers import DefaultRouter
+from . import views
+
+router=DefaultRouter()
+router.register('post',views.PostViewSet) #2개의 url 패턴을 생성해준다. 어디에? ==>router.urls에 리스트 형태로
+#리스트 형태의 url은 include를 통해 넣어준다.
 urlpatterns=[
+    path('',include(router.urls)),
 
 ]
--- PracticeDjango/instagram/views.py
+++ PracticeDjango/instagram/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
 
+from rest_framework.viewsets import ModelViewSet
+from .serializers import PostSerializer
+from .models import Post
 # Create your views here.
+
+#아래 두가지를 한번에 해주는게 ModelViewSet이다!
+# def post_list(request):
+#     pass
+# def post_detail(request,pk):
+#     pass
+
+class PostViewSet(ModelViewSet):
+    #모델 ViewSet의 기본 구성, post_list의 2개분기, Post_detail의 3개분기를 두개로 간단히 정리가능
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+    
