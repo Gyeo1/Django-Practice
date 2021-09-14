5ecfbc397d1e6251bc33e1d7a4c8fe2332d3384d
Gyeo1 <rlatjdruf99@naver.com>
2021-09-14 14:53:47 +0900

    username응답 받기

--- PracticeDjango/instagram/serializers.py
+++ PracticeDjango/instagram/serializers.py
@@ -1,7 +1,20 @@
+from django.contrib.auth import get_user_model
 from rest_framework.serializers import ModelSerializer
 from .models import Post
 
+class AuthorSerializer(ModelSerializer):
+    class Meta:
+        model=get_user_model()
+        fields=['username','email']
+
 class PostSerializer(ModelSerializer):
+    author=AuthorSerializer()
     class Meta:
         model=Post
-        fields="__all__"
+        fields=[
+            'author',
+            'pk',
+            'message',
+            'created_at',
+            'updated_at',
+        ]
