8b22603fe98c722432fc7eb95a43008d5f9f6092
Gyeo1 <rlatjdruf99@naver.com>
2021-09-14 13:50:57 +0900

    http 클라이언트 확인용 코드

--- PracticeDjango/instagram/views.py
+++ PracticeDjango/instagram/views.py
@@ -15,4 +15,8 @@ class PostViewSet(ModelViewSet):
     #모델 ViewSet의 기본 구성, post_list의 2개분기, Post_detail의 3개분기를 두개로 간단히 정리가능
     queryset = Post.objects.all()
     serializer_class = PostSerializer
+    def dispatch(self, request, *args, **kwargs): #dispatch는 요청이 올때마다 호출되는 함수
+        print("request.body: ",request.body)# logger 추천(실제 product에서는)
+        print("request.post: ", request.POST)
+        return super().dispatch(request,*args,**kwargs)
     
