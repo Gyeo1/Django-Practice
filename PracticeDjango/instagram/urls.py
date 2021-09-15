45efe00558de81fefa927b5bf9d4ae28a58dbc79
Gyeo1 <rlatjdruf99@naver.com>
2021-09-15 14:37:25 +0900

    APIView/@api_view 사용

--- PracticeDjango/instagram/urls.py
+++ PracticeDjango/instagram/urls.py
@@ -6,7 +6,7 @@ router=DefaultRouter()
 router.register('post',views.PostViewSet) #2개의 url 패턴을 생성해준다. 어디에? ==>router.urls에 리스트 형태로
 #리스트 형태의 url은 include를 통해 넣어준다.
 urlpatterns=[
-    path('public/',views.PostListAPIView.as_view()),
+    path('public/',views.post_list),
     path('',include(router.urls)),
 
 ]
--- PracticeDjango/instagram/views.py
+++ PracticeDjango/instagram/views.py
@@ -1,9 +1,12 @@
 from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework.views import APIView
 
 from rest_framework.viewsets import ModelViewSet
 from .serializers import PostSerializer
 from .models import Post
-from rest_framework  import generics
+from rest_framework import generics
 # Create your views here.
 
 #아래 두가지를 한번에 해주는게 ModelViewSet이다!
@@ -21,7 +24,28 @@ class PostViewSet(ModelViewSet):
         print("request.post: ", request.POST)
         return super().dispatch(request,*args,**kwargs)
 
-class PostListAPIView(generics.ListAPIView):
-    queryset = Post.objects.all()
-    serializer_class = PostSerializer
-post_list=PostListAPIView.as_view()
+#generic에 있는 ListAPIView로 PostList구현
+# class PostListAPIView(generics.ListAPIView):
+#     queryset = Post.objects.all().filter(is_public=True) #공개된 개시물만 가져온다.
+#     serializer_class = PostSerializer
+
+#APIView로 PostList구현하기
+# class PostListAPIView(APIView):
+#     def get(self,request):
+#         queryset = Post.objects.all().filter(is_public=True) #보여줄 데이터 쿼리셋 형식으로 가져오기
+#         serializer = PostSerializer(queryset,many=True)   #쿼리셋의 유효성 검사 및 데이터 반환
+#         return Response(serializer.data)  #직렬화 해주기
+# post_list=PostListAPIView.as_view() #class 기반 뷰에서 사용
+
+#함수기반뷰로 PostList 구현하기
+@api_view(['GET','POST']) #GET과 POST를 지원하겠다.
+def post_list(request):
+    if request.method=='GET':
+        serializer=PostSerializer(Post.objects.all().filter(is_public=True),many=True)
+        return Response(serializer.data)
+    else:#POST인 경우
+        serializer=PostSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data,status=201)
+        return Response(serializer.errors, status=400)
