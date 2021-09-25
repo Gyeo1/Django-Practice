65766e52d901d1ff51bc18464cce6e8a85378712
Gyeo1 <rlatjdruf99@naver.com>
2021-09-25 13:32:56 +0900

    json 경로 config 추가

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+
 //TODO: TODO list 구현
 function TodoList() {
   const title = '...';
@@ -7,24 +8,7 @@ function TodoList() {
 }
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return <div>Hello, React!</div>;
 }
 
 export default App;
