78b129af5925064e3ef2e3887eabaa5a5482336e
Gyeo1 <rlatjdruf99@naver.com>
2021-09-25 13:45:27 +0900

    Ant Design 적용해보기

--- src/App.js
+++ src/App.js
@@ -1,14 +1,23 @@
 import React from 'react';
+import { Button } from 'antd';
 import './App.css';
+//import "antd/dist/antd.css" //==>App.css에서 추가안할시 .js에서 이렇게 설정
 
 //TODO: TODO list 구현
-function TodoList() {
-  const title = '...';
-  const name = '---';
-}
+// function TodoList() {
+//   const title = '...';
+//   const name = '---';
+// }
 
 function App() {
-  return <div>Hello, React!</div>;
+  return (
+    <div>
+      <button>Hello, React!</button>
+      <Button type="primary" onClick={() => console.log('clicked!')}>
+        Hello, Antd!
+      </Button>
+    </div>
+  );
 }
 
 export default App;
