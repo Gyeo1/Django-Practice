f40cac6f094a13a97d555a8c98bfd14f237abbf3
Gyeo1 <rlatjdruf99@naver.com>
2021-09-26 15:36:17 +0900

    setState 실습

--- src/App.js
+++ src/App.js
@@ -8,49 +8,72 @@ import './App.css';
 //   const title = '...';
 //   const name = '---';
 // }
+const actions = {
+  init(initialValue) {
+    return { value: initialValue };
+  },
+  increment(prevState) {
+    return { value: prevState.value + 1 };
+  },
+  decrement(prevState) {
+    return { value: prevState.value - 1 };
+  },
+};
 class Counter1 extends React.Component {
-  state = {
-    value: this.props.initialValue, //props로 부터 오는 첫번째 값을 Value로 저장
-  };
-
-  onClick = () => {
-    const { value } = this.state; //현재 컴포넌트의 state 값을 value라는 상수로 받아옴
-    this.setState({ value: value + 1 }); //onClick 호출시 실행될 함수, value를 1증가시킨다.
-  };
+  state = actions.init(this.props.initialValue); //외부에서 준 initialValue 값을 action.init에 보냄
 
   render() {
     const { value } = this.state;
     return (
       <div>
         Counter1: {value}
-        <Button onClick={this.onClick}>+1</Button>
+        <Button onClick={() => this.setState(actions.increment)}>+1</Button>
+        <Button onClick={() => this.setState(actions.decrement)}>-1</Button>
       </div>
     ); //jsx 문법이다.
   }
 }
+// class Counter1 extends React.Component {
+//   state = {
+//     value: this.props.initialValue, //props로 부터 오는 첫번째 값을 Value로 저장
+//   };
 
-class FruitComponent extends React.Component {
-  render() {
-    return (
-      <div>
-        <h1>좋아하는 과일</h1>
-        <ul>
-          {this.props.fruits.map((name, index) => {
-            //외부에서 fruits 선언시 pros에서 fruits를 가져온다.
-            return <li key={index}>{name}</li>; //map으로 순회, 순회시 key라는 값을 사용해 줘라
-          })}
-        </ul>
-      </div>
-    );
-  }
-}
+//   onClick = () => {
+//     // const { value } = this.state; //현재 컴포넌트의 state 값을 value라는 상수로 받아옴
+//     // //onClick 호출시 실행될 함수, value를 1증가시킨다. setState로 상태값에 대한 변경이 가능!
+//     // this.setState({ value: value + 1 });
+//     //아니면 아래처럼 선언 가능
+//     // const value=this.state.value +1;
+//     // this.setState({value}); // value:value랑 같은데 생략 가능
+//     this.setState((preState) => {
+//       const { value } = preState;
+//       return { value: value + 1 };
+//     });
+//     this.setState((preState) => {
+//       const { value } = preState;
+//       return { value: value + 1 };
+//     });
+//     this.setState((preState) => {
+//       const { value } = preState;
+//       return { value: value + 1 };
+//     });
+//   };
+
+//   render() {
+//     const { value } = this.state;
+//     return (
+//       <div>
+//         Counter1: {value}
+//         <Button onClick={this.onClick}>+1</Button>
+//       </div>
+//     ); //jsx 문법이다.
+//   }
+// }
 
 function App() {
-  const fruits = ['바나나', '사과', '딸기'];
   return (
     <div>
       <Counter1 initialValue={10} />
-      <FruitComponent fruits={fruits} />
     </div>
   );
 }
