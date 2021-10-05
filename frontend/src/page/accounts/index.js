929d5dc589db305a0140d8ddc9afda1a1407a54b
Gyeo1 <rlatjdruf99@naver.com>
2021-10-05 22:29:01 +0900

    React로 회원가입 Form 만들기

--- frontend/src/page/accounts/index.js
+++ frontend/src/page/accounts/index.js
@@ -3,13 +3,14 @@ import { Route } from "react-router";
 //account에 관한 컴포넌트 호출시 사용한다.
 import Login from "./Login";
 import Profile from "./Profile";
-function Routes({ match }) {
+
+function AccountRoutes({ match }) {
   //match를 활용해 상위 컴포넌트에서 사용한 url에 이어 붙이기 가능
   return (
     <>
-      <Route exact path={match.url + "/profile"} component={Profile} />
-      <Route exact path={match.url + "/login"} component={Login} />
+      <Route exact={true} path={match.url + "/profile"} component={Profile} />
+      <Route exact={true} path={match.url + "/login"} component={Login} />
     </>
   );
 }
-export default Routes;
+export default AccountRoutes;
--- frontend/src/page/index.js
+++ frontend/src/page/index.js
@@ -4,15 +4,22 @@ import About from "./About";
 import Home from "./Home";
 import { Route, Switch } from "react-router-dom";
 //라우팅을 위해 사용, 매칭되는 단하나의 컴포넌트만 라우팅 시키는게 switch
-import AccountRoutes from "./accounts";
+// import AccountRoutes from "./accounts/index";
+import Signup from "./accounts/Signup";
+import Login from "./accounts/Login";
+import Profile from "./accounts/Profile";
+
 function Root() {
   return (
     <AppLayout>
+      {/* <Route exact={true} path="/accounts" component={AccountRoutes} /> */}
+      {/* 아래 3줄은 accounts 관련 경로이다. */}
+      <Route exact={true} path="/accounts/signup" component={Signup} />
+      <Route exact={true} path="/accounts/login" component={Login} />
+      <Route exact={true} path="/accounts/Profile" component={Profile} />
+
       <Route exact={true} path="/" component={Home} />
-      <Route exact path="/about" component={About} />
-      {/* <Route path="/" /> <About /> */}
-      {/* 만약 exact를 사용 안하게 된다면 ==> about으로 이동시 Home도 보여진다. */}
-      <Route exact path="/accounts" component={AccountRoutes} />
+      <Route exact={true} path="/about" component={About} />
     </AppLayout>
   );
 }
