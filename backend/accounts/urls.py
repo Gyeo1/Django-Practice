ff1747765cde8690f28bbf3aea01d96ba2bf2db0
Gyeo1 <rlatjdruf99@naver.com>
2021-10-06 14:12:00 +0900

    JWT 토근 발급을 위한 기초 작업

--- backend/accounts/urls.py
+++ backend/accounts/urls.py
@@ -1,6 +1,9 @@
-from django import urls
 from django.urls import path
 from . import views
+from rest_framework_jwt.views import obtain_jwt_token, refresh_jwt_token, verify_jwt_token
 urlpatterns = [
-    path('signup/', views.SignupView.as_view(), name='login')
+    path('signup/', views.SignupView.as_view(), name='login'),
+    path('token/', obtain_jwt_token),
+    path("token/refresh/", refresh_jwt_token),
+    path("token/verifiy/", verify_jwt_token),
 ]
